0cf7ec8fe5fc25fb4702b3f0588de3aa047e662e
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2023-02-25 15:25:57 -0500

    CNE SENIAT y bloqueo de registro de usuarios

--- src/app/main/pages/authentication/auth-register-v2/auth-register-v2.component.ts
+++ src/app/main/pages/authentication/auth-register-v2/auth-register-v2.component.ts
@@ -35,7 +35,7 @@ export class AuthRegisterV2Component implements OnInit {
     CorreoSecundario: '',
     Cargo: '',
     EsAdministrador: 0,
-    Estatus: 1,
+    Estatus: 0,
     SuspencionId: 'NULL',
     SuspencionDescripcion:  'NULL',
     UsuarioCreo: 1,
