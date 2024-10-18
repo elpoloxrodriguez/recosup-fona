82491d21512a75653865114df78519078d512391
elpoloxrodriguez <elpoloxrodriguez@gmail.com>
2024-10-18 11:12:11 -0400

    Cambio de nombre del director del FONA III

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment = {
   ID: 'ID-001',
   apiUrl: 'https://localhost',
   Hash: ':c521f27fb1b3311d686d511b668e5bd4',
-  buildDateTime: 'Fri Oct 18 2024 08:47:29 GMT-0400 (Venezuela Time)',
+  buildDateTime: 'Fri Oct 18 2024 11:11:34 GMT-0400 (Venezuela Time)',
 };
 
 
